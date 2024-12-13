776952c5e2ffd5b6c8e0a885cdaa1d73cd09a81a
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2024-12-13 16:23:46 -0300

    remove: unnecessary comments

--- src/main/java/com/use3w/grade/infra/security/SecurityConfig.java
+++ src/main/java/com/use3w/grade/infra/security/SecurityConfig.java
@@ -1,26 +1,17 @@
 package com.use3w.grade.infra.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-/**
- * Configures our application with Spring Security to restrict access to our API endpoints.
- */
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        /*
-        This is where we configure the security required for our endpoints and setup our app to serve as
-        an OAuth2 Resource Server, using JWT validation.
-        */
         return http
                 .authorizeHttpRequests((authorize) -> authorize
                     .requestMatchers("/classes").authenticated()
